b2070721af5e7b024f094b7761f38c337a0c1f49
YuYu <yuyutw123@gmail.com>
2024-11-28 16:37:01 +0800

    Fix theme problem

--- src/customize/Comment.jsx
+++ src/customize/Comment.jsx
@@ -2,66 +2,77 @@ import Giscus from '@giscus/react'
 import * as React from 'react'
 
 const id = 'inject-comments'
+let newTheme = 'auto'
 
+function getSystemTheme() {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
 // Retrieve the theme configuration from localStorage
 function getSavedTheme() {
-    return window.localStorage.getItem('theme') || 'dark'
+  return window.localStorage.getItem('theme') || 'dark'
+}
+
+function checkTheme() {
+  newTheme = getSavedTheme()
+  if (newTheme === 'auto') newTheme = getSystemTheme()
+  return newTheme
 }
 
 const Comments = () => {
-    const [mounted, setMounted] = React.useState(false)
-    const [theme, setTheme] = React.useState('light')
+  const [mounted, setMounted] = React.useState(false)
+  const [theme, setTheme] = React.useState('dark')
 
-    // Effect to monitor `localStorage` changes
-    React.useEffect(() => {
-        const handleStorageChange = event => {
-            if (event.key === 'theme') {
-                setTheme(event.newValue || 'light')
-            }
+  // Effect to monitor `localStorage` changes
+  React.useEffect(() => {
+    const handleStorageChange = event => {
+      if (event.key === 'theme') {
+        if (event.newValue === 'auto') {
+          setTheme(getSystemTheme())
+        } else {
+          setTheme(event.newValue || 'light')
         }
+      }
+    }
 
-        // Add event listener for storage changes
-        window.addEventListener('storage', handleStorageChange)
+    // Add event listener for storage changes
+    window.addEventListener('storage', handleStorageChange)
 
-        // Initial update
-        setTheme(getSavedTheme())
+    // Initial update
+    setTheme(checkTheme())
 
-        return () => {
-            // Cleanup listener on unmount
-            window.removeEventListener('storage', handleStorageChange)
-        }
-    }, [])
+    return () => {
+      // Cleanup listener on unmount
+      window.removeEventListener('storage', handleStorageChange)
+    }
+  }, [])
 
-    // Ensure Giscus component is rendered only after the component is mounted
-    React.useEffect(() => {
-        setMounted(true)
-    }, [])
+  // Ensure Giscus component is rendered only after the component is mounted
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
 
-    return (
-        <div id={id} className='w-full'>
-            {mounted ? (
-                <Giscus
-                    id={id}
-                    repo='YuYutw123/YuYutw123.github.io'
-                    repoId='R_kgDONUsJVg'
-                    category='General'
-                    categoryId='DIC_kwDONUsJVs4CkrUq'
-                    mapping='title'
-                    reactionsEnabled='1'
-                    emitMetadata='0'
-                    inputPosition='top'
-                    lang='en'
-                    loading='lazy'
-                    theme={theme}
-                />
-            ) : null}
-        </div>
-    )
+  return (
+    <div id={id} className='w-full'>
+      {mounted ? (
+        <Giscus
+          id={id}
+          repo='YuYutw123/YuYutw123.github.io'
+          repoId='R_kgDONUsJVg'
+          category='General'
+          categoryId='DIC_kwDONUsJVs4CkrUq'
+          mapping='title'
+          reactionsEnabled='1'
+          emitMetadata='0'
+          inputPosition='top'
+          lang='en'
+          loading='lazy'
+          theme={theme}
+        />
+      ) : null}
+    </div>
+  )
 }
 
 export default Comments
-
-// Author：liruifengv李瑞丰
-// Link：https://juejin.cn/post/7359405432802607167
-// Source：稀土掘金
-// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
