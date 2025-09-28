7d910ad98699d7e6587aba1886295e64423ec614
YuYu <yuyutw123@gmail.com>
2025-09-28 17:27:40 +0800

    fix home page background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,16 @@ import Blog from "./blog/page";
 
 export default function Profile() {
     return (
-        <main className="flex flex-1 justify-center py-12 sm:px-6 lg:px-8 bg-gray-50">
+        <main className="min-h-screen flex flex-1 justify-center pt-12 sm:px-6 lg:px-8 bg-gray-50">
             <div className="w-full max-w-4xl space-y-12">
                 {/* Header */}
                 <ProfileHeader
                     name="YuYutw123"
-                    titles={["普通和理所當然是什麼呢", "我也不知道", "為什麼要演奏春日影"]}
+                    titles={[
+                        "普通和理所當然是什麼呢",
+                        "我也不知道",
+                        "為什麼要演奏春日影",
+                    ]}
                     avatarUrl="https://cdn.discordapp.com/avatars/338661569384808458/c8423a79902bb2e37aa4620d91129862.webp?size=1280"
                 />
                 <Blog />
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,11 @@
 export default function Navbar() {
     return (
-        <footer className="text-gray-700 text-xs py-2 mt-8 border-1 border-t-blue-200 shadow shadow-blue-200">
+        <footer className="text-gray-700 text-xs py-2 border-t border-blue-200 shadow shadow-blue-200">
             <div className="container mx-auto text-center">
-                <p>&copy; {new Date().getFullYear()} YuYutw123&apos;s Blog. All rights reserved.</p>
+                <p>
+                    &copy; {new Date().getFullYear()} YuYutw123&apos;s Blog. All
+                    rights reserved.
+                </p>
             </div>
         </footer>
     );
